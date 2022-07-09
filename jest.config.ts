5bca8bbff07d7fc5a57bbd14931e05d76de4c2d5
evalton <evalton.nunes@gmail.com>
2022-07-09 17:08:52 -0300

    Alterações para correção ci

--- jest.config.ts
+++ jest.config.ts
@@ -1,4 +1,3 @@
-import {pathsToModuleNameMapper} from 'ts-jest/utils';
 import {compilerOptions} from './tsconfig.json';
 
 export default {
@@ -77,9 +76,9 @@ export default {
   // ],
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
-   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths,
+/*   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths,
      {prefix: '<rootDir>/'
-  }),
+  }),*/
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
